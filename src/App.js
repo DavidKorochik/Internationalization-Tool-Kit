b7489d9bb0c8e20c04493d80ef7be4adfc11cc51
DavidKorochik <david1korochik@gmail.com>
2021-11-20 15:20:55 +0200

    Import the Home.js component and displayed it in our app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      {/* <h1>Hello World!</h1> */}
+      <Home />
     </div>
   );
 }
