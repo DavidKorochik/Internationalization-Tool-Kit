71407c03079498c8511bbde434545dc3868c4f62
DavidKorochik <david1korochik@gmail.com>
2021-11-20 16:27:41 +0200

    Created initial input

--- src/components/LanguagesInput.js
+++ src/components/LanguagesInput.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AutoComplete } from 'antd';
 
 export default function LanguagesInput() {
-  return <div></div>;
+  const [value, setValue] = useState('');
+  const [options, setOptions] = useState([]);
+
+  const onSearch = (searchText) => {
+    setOptions('');
+  };
+
+  const onSelect = (data) => {
+    console.log('onSelect', data);
+  };
+
+  const onChange = (data) => {
+    setValue(data);
+    console.log(value);
+  };
+
+  return (
+    <>
+      <AutoComplete
+        options={options}
+        style={{
+          width: 500,
+          marginLeft: '30%',
+          border: '1px solid black',
+        }}
+        onSelect={onSelect}
+        onSearch={onSearch}
+        onChange={onChange}
+        placeholder='Search a language'
+      />
+      <button style={{ marginLeft: '30px' }}>Add Language</button>
+    </>
+  );
 }
