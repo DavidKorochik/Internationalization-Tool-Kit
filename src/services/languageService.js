e34e43da182d281f7052d9ac9d2298b85629f77d
DavidKorochik <david1korochik@gmail.com>
2021-11-20 15:54:39 +0200

    added a mock language list and commented out the axios requests

--- src/services/languageService.js
+++ src/services/languageService.js
@@ -1,3 +1,42 @@
-const languageService = {};
+import axios from 'axios';
+
+const languageService = {
+  getLanguages: () => {
+    //   axios.get(`${server}/words`);
+    return new Promise((res, rej) => {
+      try {
+        res([
+          {
+            languageId: 1,
+            language: 'English',
+            abbreviation: 'en',
+          },
+          {
+            languageId: 2,
+            language: 'Hebrew',
+            abbreviation: 'he',
+          },
+        ]);
+      } catch (err) {
+        rej(err);
+      }
+    });
+  },
+  saveLanguage: () => {
+    //   axios.post(`${server}/words`, data)
+  },
+  deleteSavedLanguage: () => {
+    // axios.delete(`${server}/words/${id}`);
+  },
+  addWordTranslation: () => {
+    // axios.post(`${server}/words/translation`, data);
+  },
+  getWordTranslation: () => {
+    // axios.get(`${server}/words/translation`);
+  },
+  getGoogleTranslateTranslation: () => {
+    // axios.post(`${server}/googletranslate`);
+  },
+};
 
 export default languageService;
