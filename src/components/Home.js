8fff8bf7d7120da417c49a3e90f5879b251f4976
DavidKorochik <david1korochik@gmail.com>
2021-11-20 15:28:35 +0200

    imported LanguagesInput.js component in Home.js component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import LanguagesInput from './LanguagesInput';
 
 export default function Home() {
   return (
     <div>
       <h1>Home Page!</h1>
+      <LanguagesInput />
     </div>
   );
 }
