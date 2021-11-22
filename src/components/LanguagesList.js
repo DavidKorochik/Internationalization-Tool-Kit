492130b59a348a561577ca2e6ee2e80c89095114
DavidKorochik <david1korochik@gmail.com>
2021-11-22 06:39:57 +0200

    Displayed the languages

--- src/components/LanguagesList.js
+++ src/components/LanguagesList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import languageService from '../services/languageService';
+import { Card, Col, Row, Divider } from 'antd';
 
 export default function LanguagesList() {
   const [languages, setLanguages] = useState([]);
@@ -18,12 +19,26 @@ export default function LanguagesList() {
       >
         Languages List
       </h1>
-      {languages.map((lang) => (
-        <div>
-          <h2>{lang.language}</h2>
-          <h3>{lang.abbreviation}</h3>
-        </div>
-      ))}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          flexFlow: 'wrap',
+        }}
+      >
+        {languages.map((lang, index) => (
+          <div
+            key={index}
+            style={{
+              padding: '30px',
+            }}
+          >
+            <Card title={lang.language} bordered={false} style={{ width: 300 }}>
+              <h3>{lang.abbreviation}</h3>
+            </Card>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
